a9edfb1b31ff56e789eff8595740815a60476e08
cezar-parladore <cezarparladore@outlook.com>
2022-04-03 14:28:57 -0300

    prototipo funcionando

--- ALS_tables.py
+++ ALS_tables.py
@@ -4,13 +4,12 @@ from glob import glob
 import easygui
 
 input_folder = easygui.diropenbox()
-input_files = glob(input_folder+"/*.xlsx")
-
+input_files_path = glob(input_folder+"/*.xlsx")
 class ALS_file:
     # TODO path must be mandatory
     def __init__(self, path_to_file: str) -> None:
-        self.path_to_files = path_to_file
-        self.file_name = path_to_file.split("/")[-1]
+        self.path_to_file = path_to_file
+        self.file_name = path_to_file.split("/")[-1].replace(".xlsx", "")
         self.worksheets = {
             "Cliente":'',
             "Coleta":'',
@@ -19,38 +18,36 @@ class ALS_file:
             "QAQC":'',
             "Dil":''
         }
-        pass
 
 
     def __str__(self) -> str:
         return self.file_name
 
 
-    def get_worksheets(self):
+    def get_worksheets(self, tables_only:bool=True):
         for key in self.worksheets.keys():
             print(key)
-            self.worksheets[key] = pd.read_excel(
-                self.path_to_file,
-                sheet_name=key
-            )
+            if tables_only:
+                self.worksheets[key] = pd.read_excel(
+                    self.path_to_file,
+                    sheet_name=key,
+                    skiprows=9
+                )
+            else:
+                self.worksheets[key] = pd.read_excel(
+                    self.path_to_file,
+                    sheet_name=key
+                )
+
             #TODO implemment check if is corrupt; then call fix corrupt xls
+    def check_qaqc(self):
+        qaqc_ws = self.worksheets['QAQC']
+        return qaqc_ws
 
+arquivo = ALS_file()
+arquivo.get_worksheets()
+arquivo.check_qaqc()
 
+    #ANCHOR - def check_qaqc
+    #ANCHOR - def to RESUMO
 
-files_dict ={}
-
-for file in files
-
-    
-glob(input_files)
-
-
-
-worksheets = {
-            "Cliente":'',
-            "Coleta":'',
-            "Metodos":'',
-            "Resultados":'',
-            "QAQC":'',
-            "Dil":''
-        }
